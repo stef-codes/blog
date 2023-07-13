1fd5235a0f7a55ee6e8a39c668a55756ce901533
Stefanie <stefcodes@gmail.com>
2023-07-13 15:25:59 -0700

    change metadata description

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'Stefanie Davis',
   author: 'Stefanie Davis',
   headerTitle: 'Stefanie Davis',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Stefanie Davis Blog that showcases technical thoughts and musings',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
