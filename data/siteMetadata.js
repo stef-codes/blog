d78cb71f4fc767b723c5e9b7fab8f069a33cb80c
Stefanie <stefcodes@gmail.com>
2023-07-13 15:24:01 -0700

    change copy to my name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Stefanie Davis',
   author: 'Stefanie Davis',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'Stefanie Davis',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  // siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
